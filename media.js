d47f6f4a881fb3ce8ee4939b9158fc0ec91e6afa
Mehmet AYDIN <mehmetaydin51@gmail.com>
2015-12-04 17:15:25 +0200

    NOW DOES NOT REQUIRES JQUERY

--- media.js
+++ media.js
@@ -1,35 +1,44 @@
 // media.js
 // by theDeveloper51
-// requires jQuery
 function media(px,func1,func2){
-	var newpx = parseInt(px.slice(1));
-	var bigsmall = px.slice(0,1);
+	function width(){
+		if(typeof(window.innerWidth) == 'number'){
+			return window.innerWidth;
+		}else if(document.documentElement.clientWidth){
+			return document.documentElement.clientWidth;
+		}else if(document.body.clientWidth){
+			return document.body.clientWidth;
+		}
+	}
+	var newpx = parseInt(px.slice(1));	//PX
+	var bigsmall = px.slice(0,1);		//Bigger or Smaller
+
 	if(bigsmall == '>'){
-		if($(window).width() > newpx){
+		if(width() > newpx){
 				func1();
 		}else{
 				func2();
 		}
-		$(window).resize(function(){
-			if($(window).width() > newpx){
+		window.onresize = function(){
+			if(width() > newpx){
 				func1();
 			}else{
 				func2();
 			}
-		});
+		}
 	}else if(bigsmall == '<'){
-		if($(window).width() < newpx){
+		if(width() < newpx){
 				func();
 		}else{
 				func2();
 		}
-		$(window).resize(function(){
-			if($(window).width() < newpx){
+		window.onresize = function(){
+			if(width() < newpx){
 				func1();
 			}else{
 				func2();
 			}
-		});
+		}
 	}else{
 		console.log('WRONG!!!');
 	}
